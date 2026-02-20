c991d2042d82e0f17bf4b14a61d91c339bbc1d1c
Eliubentus <eliubergonzalez@gmail.com>
2026-02-20 15:59:06 -0400

    implementacion de player a clase abstracta

--- src/main/java/com/amongus/core/model/Player.java
+++ src/main/java/com/amongus/core/model/Player.java
@@ -1,24 +1,37 @@
 package com.amongus.core.model;
 
-public class Player {
-    private String name;
-    private String color;
+public abstract class Player{
+    private final String name;
+    private final String color;
     private Position position;
+    private boolean isAlive;
 
     public Player(String name, String color){
         this.name=name;
         this.color=color;
-
+        position=new Position(0,0);
+        isAlive=true;
     }
 
     public String getName() {
         return name;
     }
 
-    public void move(int deltaX, int deltaY){
-
-
+    public String getColor() {
+        return color;
     }
 
+    public Position getPosition() {
+        return position;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void move(int deltaX, int deltaY){
+        this.position=new Position(this.position.x()+deltaX,this.position.y()+deltaY);
+    }
+    public abstract String getRol();
 
 }
