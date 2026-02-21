aedc6be31d6cd64d9df36ebd147a0aafcacc0b7d
Eliubentus <eliubergonzalez@gmail.com>
2026-02-21 00:11:02 -0400

    implementacion de player clase abstracta

--- src/main/java/com/amongus/core/model/Player.java
+++ src/main/java/com/amongus/core/model/Player.java
@@ -1,24 +1,34 @@
 package com.amongus.core.model;
 
 public abstract class Player{
-    private final String name;
-    private final String color;
+    private final PlayerId id;
+    private String name;
+    private String color;
     private Position position;
     private boolean isAlive;
 
     public Player(String name, String color){
+        this.id=PlayerId.random();
         this.name=name;
         this.color=color;
-        position=new Position(0,0);
-        isAlive=true;
+        position=new Position(0,0);//Lobby
+        isAlive=true;//Jugador vivo
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public Position getPosition() {
@@ -29,9 +39,15 @@ public abstract class Player{
         return isAlive;
     }
 
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
     public void move(int deltaX, int deltaY){
+        //El jugador se esta moviendo
         this.position=new Position(this.position.x()+deltaX,this.position.y()+deltaY);
     }
+
     public abstract String getRol();
 
 }
